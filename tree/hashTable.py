ce2070609f10919579248e5a5c5d3f9ce0788b6c
Unknown <emilys2costa@gmail.com>
2019-05-01 08:39:39 -0300

    just theory

---
+++ tree/hashTable.py
@@ -0,0 +1,16 @@
+#Material de Apoio: https://blog.pantuza.com/artigos/tipos-abstratos-de-dados-tabela-hash
+
+#Operacoes#
+'''
+1 - Inicilizar a estrutura de dados
+2 - Pesquisar um ou mais registros com uma dada chava
+3 - Inserir um novo registro
+4 - Remover um registro específico
+5 - Ordenar os registros
+'''
+
+#Filosofias#
+'''
+(1) -> Hashing Fechado ou de endereçamento aberto (funciona muito bem se o univero analisado é pequeno)
+(2) -> Hashing Aberto ou encadeamento
+'''
